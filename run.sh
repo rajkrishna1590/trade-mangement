4d11cdb019487dce383487bcbea3eea4a2a46b92
Portal Developer <dev-portal@talabs.net>
2019-02-20 19:09:29 +0530

    mongo connect issue fixed

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 echo -e "\e[32mClean up before rebuilding the trade app containers\e[0m"
 docker-compose down 
 echo -e "\e[32mClean up before rebuilding the trade app containers\e[0m"
-#docker-compose build  
+docker-compose build  
 echo -e "\e[32mBringing up the trade app containers\e[0m"
 docker-compose up -d
