8a6d8ef06d50eb37af239363a4a52b9bea0cb927
Portal Developer <dev-portal@talabs.net>
2019-02-22 02:30:51 +0530

    adding mocha and docker fixes for test case running

--- mongo-seed/import.sh
+++ mongo-seed/import.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sleep 30
-mongoimport --username=trade --password=trade123 --authenticationDatabase=admin --db=trade --collection=user1 --type=json --file=/mongo-seed/user.json --jsonArray
+mongoimport --username=trade --password=trade123 --authenticationDatabase=admin --db=trade --collection=user --type=json --file=/mongo-seed/user.json --jsonArray
 mongoimport --username=trade --password=trade123 --authenticationDatabase=admin --db=trade --collection=stock --type=json --file=/mongo-seed/stock.json --jsonArray
--- test/testRunner.sh
+++ test/testRunner.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # stop on error
 set -e
@@ -16,91 +16,8 @@ TEST_ENV="test/test.env"
 source "$TEST_ENV"
 export $(cut -d= -f1 "$TEST_ENV")
 
-usage() {
-        cat <<EOF
-Usage: testRunner.sh [option] [value]
-Runs mocha and generates coverage report
-Without any options, all files inside ./test/ will run
-Coverage report will be saved in ./.coverage
+MOCHA_CMD_ARGS="test --recursive"
 
-Options:
-=======
-  -f [filename] - tests only the file
-  -m [module]   - run all the available functions
-  -d            - attaches node debugger
-  -h            - show this help message and exit
-
-Examples:
-========
-  testRunner.sh -f test/models/cinder/cinderAPITest.js
-  testRunner.sh -df test/models/ceph/cephAPITest.js
-  testRunner.sh -f test/models/ceph/cephAPITest.js -f test/controllers/networks/routerControllerTest.js
-  testRunner.sh -m controllers
-  testRunner.sh -dm models
-EOF
-}
-
-modules=()
-files=()
-files_for_report=()
-
-while getopts ":dhm:f:o:" opt; do
-    case "${opt}" in
-        d)
-            debug=true
-            ;;
-        o)
-            OTHER_OPTS=$OPTARG
-            ;;
-        m)
-            modules+=(${OPTARG})
-            ;;
-        f)
-            files+=(${OPTARG})
-            files_for_report+=($(echo $OPTARG | sed 's#\(test/\)\(.*\)\(Test.js\)#\2.js#'))
-            ;;
-        h)
-            usage
-            exit
-            ;;
-        *)
-            echo "illegal option -$OPTARG"
-            usage
-            exit 1
-            ;;
-    esac
-done
-shift "$((OPTIND-1))"
-
-#Run the test
-if [[ ${#files[@]} != 0 ]];
-then
-    if [[ ${#files[@]} == 1 ]];
-    then
-        glob_pattern_report="${files_for_report[@]}"
-        glob_pattern="${files[@]}"
-    else
-        glob_pattern_report={${files_for_report[@]}}
-        glob_pattern={${files[@]}}
-    fi
-    glob_pattern_nyc="${glob_pattern_report// /,}"
-    glob_pattern_mocha="${glob_pattern// /,}"
-    MOCHA_CMD_ARGS="$glob_pattern_mocha"
-elif [[ ${#modules[@]} != 0 ]];
-then
-    if [[ ${#modules[@]} == 1 ]];
-    then
-        glob_pattern="${modules[@]}"
-    else
-        glob_pattern={${modules[@]}}
-    fi
-    glob_pattern_nyc="${glob_pattern// /,}"
-    glob_pattern_mocha="test/${glob_pattern// /,}"
-    MOCHA_CMD_ARGS="--recursive $glob_pattern_mocha"
-
-else
-    MOCHA_CMD_ARGS="test --recursive"
-fi
 
 NYC_CMD_ARGS="--report-dir .coverage --reporter=html"
 
