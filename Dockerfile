e3e451636b81a86d5dafc8b01d90704fd1d7e6e8
Portal Developer <dev-portal@talabs.net>
2019-02-22 00:28:08 +0530

    WIP : Environment start issue fixed

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
 FROM node:8.9.4-alpine
 RUN mkdir trade-app
 WORKDIR trade-app  
-RUN npm install 
-#RUN npm run eslint
-CMD ["npm", "start"]
--- app-entry.sh
+++ app-entry.sh
@@ -2,6 +2,6 @@
 hostip=$(ip route show | awk '/default/ {print $3}')
 echo "{\"host\": \"$hostip\"}"  
 sleep 10
-#npm i
+npm i
 node main.js
 tail -F -n0 /etc/hosts
