89a9ca5b8322941b5b240080cac07488f5de3b73
Portal Developer <dev-portal@talabs.net>
2019-02-22 02:44:42 +0530

    data seeding bug fixed

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ echo -e "\e[32mClean up before rebuilding the trade app containers\e[0m"
 docker-compose build  
 echo -e "\e[32mBringing up the trade app containers\e[0m"
 docker-compose up -d
+docker exec -it trade-app-db sh /mongo-seed/import.sh
